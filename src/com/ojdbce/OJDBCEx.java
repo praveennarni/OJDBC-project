babc7b9aca1cb8efa46398fd72a0c40dabb20a71
praveennarni <praveenbabunarni1999@gmail.com>
2025-04-11 11:27:41 -0500

    Added changes to the code

--- src/com/ojdbce/OJDBCEx.java
+++ src/com/ojdbce/OJDBCEx.java
@@ -1,5 +1,6 @@
 package com.ojdbce;
 import java.sql.*;
+import java.util.Scanner;
 		public class OJDBCEx {
 			public static void main(String[] args) throws Exception {
 
@@ -10,6 +11,8 @@ import java.sql.*;
 				String username = "SYSTEM";
 				String password = "root";
 
+				Scanner scn = new Scanner(System.in);
+				
 				//step 1: Register the driver
 				Class.forName(driver);
 
@@ -17,22 +20,39 @@ import java.sql.*;
 					Connection con = DriverManager.getConnection(url,username,password);
 
 					 System.out.println("Connected to the database!");
+					 
+					 
+					 String sql="insert into student(ID,name) VALUES (?,?)";
+					 PreparedStatement pstmt=con.prepareStatement(sql);
+					 
+					 System.out.print("Enter your ID: ");
+					 int ID= scn.nextInt();
+					 scn.nextLine();
+					 
+					 System.out.println("Enter your Name: ");
+					 String name=scn.nextLine();
+					 
+					 pstmt.setInt(1,ID);
+					 pstmt.setString(2,name);
+					 
+					 int rowsInserted = pstmt.executeUpdate();
+					 if (rowsInserted >0) {System.out.println("Data Saved!");}
 					 		            
 					//step 3: create the statement object
-					Statement stmt = con.createStatement();
+					//Statement stmt = con.createStatement();
 
 					//step 4: execute the querie
 
-
+/*
 					stmt.executeUpdate("insert into student values(1,'Babu')");
 					stmt.executeUpdate("insert into student values(2,'Praveen')");
 					stmt.executeUpdate("insert into student values(3,'Narni')");
 					//stmt.executeUpdate("insert into student values(1, 'Babu')");
 
-
-					 //scanner.close();
+*/
+					 scn.close();
 					 //statement.close();
-
+pstmt.close();
 					//step 5: close the conncetion
 					con.close();
 	}
